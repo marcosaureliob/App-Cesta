ee8a85394bd77ff2b32e30463377892b99748459
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-08-28 02:01:49 -0300

    Projeto final

--- src/componentes/Texto.js
+++ src/componentes/Texto.js
@@ -1,11 +1,23 @@
-import { Text } from "react-native";
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
 
-export default function Texto({children, style}) {
-    return <Text style={[style, estilos.texto]}>{children}</Text>
+export default function Texto({ children, style }) {
+  let estilo = estilos.texto;
+  
+  if(style?.fontWeight === 'bold') {
+    estilo = estilos.textoNegrito;
+  }
+
+  return <Text style={[style, estilo]}>{ children }</Text>
 }
 
 const estilos = StyleSheet.create({
-    texto: {
-        fontFamily: "MontserratRegular"
-    }
-})
+  texto: {
+    fontFamily: 'MontserratRegular',
+    fontWeight: 'normal',
+  },
+  textoNegrito: {
+    fontFamily: 'MontserratBold',
+    fontWeight: 'normal',
+  }
+});
