ff37916dbff113496b62e465483d37ca18708dfc
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-08-28 01:48:30 -0300

    Commit inicial

---
+++ src/componentes/Texto.js
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export default function Texto({children, style}) {
+    return <Text style={[style, estilos.texto]}>{children}</Text>
+}
+
+const estilos = StyleSheet.create({
+    texto: {
+        fontFamily: "MontserratRegular"
+    }
+})
